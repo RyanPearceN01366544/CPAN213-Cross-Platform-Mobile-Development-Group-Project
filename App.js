e757d295ee2a88e9ce0e371f738e7ca6f4b990b8
LuckyNinja1 <156844535+RyanPearceN01366544@users.noreply.github.com>
2025-04-03 17:49:00 -0400

    A lot of Fixes

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { Provider } from 'react-redux';
 import store from "./redux/store"
+import AppNavigator from './AppNavigator';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <AppNavigation/>
+      <AppNavigator/>
       <StatusBar style="auto" />
     </Provider>
   );
--- screens/CalenderScreen.js
+++ screens/CalenderScreen.js
@@ -1,10 +1,10 @@
 import { Text, View } from "react-native"
 
-const CallendarScreen = () => {
+const CalenderScreen = () => {
     return (
         <View>
             <Text>Callendar Screen</Text>
         </View>
     )
 }
-export default CallendarScreen;
+export default CalenderScreen;
