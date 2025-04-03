6d56184654f81420a426aa729a783286bb53361c
LuckyNinja1 <156844535+RyanPearceN01366544@users.noreply.github.com>
2025-04-03 17:17:35 -0400

    Added Basic Screens for Each Home, Calendar, Settings and Tasks for testing.

---
+++ screens/CalenderScreen.js
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native"
+
+const CallendarScreen = () => {
+    return (
+        <View>
+            <Text>Callendar Screen</Text>
+        </View>
+    )
+}
+export default CallendarScreen;
