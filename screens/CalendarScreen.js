dc57cda1c1a88781ec6c7c83e8c444952549d5bc
LuckyNinja1 <156844535+RyanPearceN01366544@users.noreply.github.com>
2025-04-04 09:06:53 -0400

    Removed Calender (It haunts me.) Added a decent amount to CalendarScreen.

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -1,10 +1,53 @@
+import { useState } from "react";
 import { Text, View } from "react-native"
 
-const CalendarScreen = () => {
+const CallendarScreen = () => {
+    const [timeNodes, setTimeNodes] = useState();
+    // -- Todo -- 
+    // 1. Make styles to make calendar look good.
+    // 2. Make temporary variables for testing.
+    // 3. COnnect to Firebase
+
+    const emptyTimeComponent = () => { // Todo: Make block still extend out.
+        return(
+            <View></View>
+        )
+    }
+    const timeComponent = (id) => {
+        
+        return(
+            <View>
+
+            </View>
+        )
+    }
+    function isTimeEmpty(endTime)
+    {
+        let beginLengthHour = 0; // The hour of the next timeComponent.
+        let beginLengthMinute = 0; // The minute of the next timeComponent.
+
+        
+    }
+    const weekComponent = (weekday) => { // Will read the week and fill out per-day.
+        
+        return(
+            <View>
+                <Text>{weekday}</Text>
+                {
+                    timeNodes.map((node) => {
+                        
+                    })
+                }
+            </View>
+        )
+    }
+
     return (
         <View>
-            <Text>Calendar Screen</Text>
+            <View>
+                
+            </View>
         </View>
     )
 }
-export default CalendarScreen;
+export default CallendarScreen;
